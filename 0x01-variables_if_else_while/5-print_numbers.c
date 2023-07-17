d9c608718ad07179e7f8ac6c4c56abd0cc4d24d8
Getayawkal Tsegaye <getayawukalz@gmail.com>
2023-07-17 04:49:25 -0700

    Task5Cpa

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers 0 to 9.
+ *
+ * Return: 0 eachtime (Success)
+ */
+int main(void)
+{
+	int zerotonine;
+
+	for (zerotonine = 0; zerotonine < 10; zerotonine++)
+	{
+		printf("%d", zerotonine);
+	}
+	putchar('\n');
+	return (0);
+}
