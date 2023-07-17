3783349eac82ff40fe3abe3a416432a2c3855690
Getayawkal Tsegaye <getayawukalz@gmail.com>
2023-07-17 02:57:03 -0700

    Task3Cpb

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Prints the alphabet in lower and upper case
  *
  * Return: 0 each time
  */
-int main() 
+int main(void)
 {
-	char alp [52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
