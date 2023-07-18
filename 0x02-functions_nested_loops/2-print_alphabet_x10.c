d6ab6a7a0a71bd5caea021c390d0a9c8980d2787
Getayawkal Tsegaye <getayawukalz@gmail.com>
2023-07-18 05:56:43 -0700

    ThreeTasksDonefnla

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Prints 10x lower case alphabt
+ *
+ * Return: 0 eachtime
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
