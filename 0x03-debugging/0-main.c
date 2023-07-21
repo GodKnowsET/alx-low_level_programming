3a2a120acb05e7901332401ecc2e5d734a3b00fc
Getayawkal Tsegaye <getayawukalz@gmail.com>
2023-07-21 02:31:43 -0700

    Task0da

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - This code test for positive or negative functions
+* Return: 0 eachtime
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
